a2916113c3d3b8683db4912f0bc4baa7e2659143
LarisaSam <66268481+LarisaSam@users.noreply.github.com>
2020-07-19 19:48:35 +0400

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -6,7 +6,7 @@ result = {'Зима': (1, 2, 12),
           'Лето': (6, 7, 8),
           'Осень': (9, 10, 11)}
 
-num_month = int(input("Введите номер месяца - число от 0 до 12"))
+num_month = int(input("Введите номер месяца - число от 1 до 12"))
 for key in result.keys():
     if num_month in result[key]:
         print(key)
